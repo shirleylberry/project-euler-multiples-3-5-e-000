9c86c986e4c976175259db7b38b6aeff75bdac50
shirleylberry <shirleylberry@gmail.com>
2015-12-08 00:22:59 -0500

    procedural solution for multiples

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,16 @@
 # Enter your procedural solution here!
+def multiples_3_5(max_num)
+	total = 0
+	for number in 1...max_num
+		if number % 3 == 0
+			total += number
+		elsif number % 5 == 0
+			total += number
+		end
+	end
+	return total
+end
+
+sum_nums = multiples_3_5(1000)
+
+puts sum_nums
